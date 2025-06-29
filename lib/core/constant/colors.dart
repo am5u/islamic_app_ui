6101ba9367cf94315d96b3cb097ef38afa250d5f
Ammar Mohamed <ammareldesouki130@gmail.com>
2025-06-30 01:49:34 +0300

    just start project with assets

---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+abstract class TColors {
+  static const Color primaryColor = Color(0xffE2BE7F);
+  static const Color textWhite= Color(0xffFFFFFF);
+  static const Color textBlack = Color(0xff000000);
+}
---
+++ lib/core/theme/wedgits_theme/bottom_sheet_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:etqan_acadmy/utils/constants/colors.dart';
+
+class TBottomSheetTheme {
+  static BottomSheetThemeData lightBottomSheetTheme = BottomSheetThemeData(
+    showDragHandle: true,
+    backgroundColor: TColors.white,
+    modalBackgroundColor: TColors.white,
+    constraints: const BoxConstraints(minWidth: double.infinity),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+  );
+
+
+    static BottomSheetThemeData darkBottomSheetTheme = BottomSheetThemeData(
+    showDragHandle: true,
+    backgroundColor: TColors.black,
+    modalBackgroundColor: TColors.black,
+    constraints: const BoxConstraints(minWidth: double.infinity),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+  );
+}
